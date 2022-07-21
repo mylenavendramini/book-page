f6144f906bd78e18e5c5c17a0489c7d4263cfe86
mylenavendramini <myvendramini@gmail.com>
2022-07-21 16:27:47 +0100

    delete context folder

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BooksProvider } from "./contexts/books.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <BooksProvider>
-        <App />
-      </BooksProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
